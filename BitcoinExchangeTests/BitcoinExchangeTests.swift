4f52e6bf91a64ff0c73820f9ade92087f313b104
jebolivar <brolivars@gmail.com>
2024-12-19 19:37:16 +0100

    Initial Commit

---
+++ BitcoinExchangeTests/BitcoinExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinExchangeTests.swift
+//  BitcoinExchangeTests
+//
+//  Created by Jose Bolivar on 19/12/24.
+//
+
+import Testing
+@testable import BitcoinExchange
+
+struct BitcoinExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
